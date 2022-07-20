a6e9f4f05d3f6e7726bdadf6724d00a0f1f8a2b5
Dakota Hanks <dakotahanks1020@gmail.com>
2022-07-20 17:34:44 -0400

    add lightbox functionality and gallery function

--- javascript/index.js
+++ javascript/index.js
@@ -37,9 +37,82 @@ let itemNum = document.getElementById("item-num");
 function increment() {
     itemCount += 1;
     itemNum.innerHTML = `${itemCount}`;
+    return itemCount;
 }
 
 function decrement() {
     itemCount -= 1;
     itemNum.innerHTML = `${itemCount}`;
+    return itemCount;
+}
+//remove empty text when cart has more than 0 items inside it and replace it with that item and its info
+// const cartButton = document.getElementById("add-to-cart");
+const empty = document.getElementById("empty");
+const cartItem = document.getElementById("cart-item");
+const checkout =  document.getElementById("checkout");
+const quantity = document.getElementById("item-quantity");
+const price = document.getElementById("total-price");
+
+function addToCart() {
+    if (itemCount > 0) {
+        // remove empty text
+        empty.style.display = "none";
+
+        // replace empty text with cart-item and checkout button
+        cartItem.style.display = "block";
+        checkout.style.display = "block";
+
+        quantity.innerHTML = `${itemCount}`;
+
+        let x = 125 * itemCount;
+        price.innerHTML = `$${x}`;
+    } else {
+        // make empty text re-appear if item count is 0 or below
+        cartItem.style.display = "none";
+        checkout.style.display = "none";
+        empty.style.display = "block";
+    }
+}
+
+// add lightbox functionality
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.querySelectorAll("img.mySlide");
+  const dots = document.querySelectorAll("img.preview");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+//console.log(slides[0]);
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// change large img on desktop by clickin on thumbnail imgs
+function changeImg(n) {
+    const thumbnails = document.querySelectorAll("img.main-preview");
+    const productImages = document.querySelectorAll("img.product-img");
+    const mainImg = document.getElementById("main-img");
+
+    mainImg.setAttribute("src", `${productImages[n].src}`);
+    thumbnails[n].className += " active";
+
+
 }
